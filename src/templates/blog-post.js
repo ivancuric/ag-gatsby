a7dc42872667944dbde1d74a1d0e6d16f7181722
Ivan Čurić <ivan.curic@gmail.com>
2018-04-22 14:37:55 +0200

    Remove tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
 import Content, { HTMLContent } from '../components/Content';
 
 export const BlogPostTemplate = ({
