68920263d439c00d7539594dae4eb48a8be87bd0
Ivan Čurić <ivan.curic@gmail.com>
2018-07-03 23:57:07 +0200

    remove refs to cms.js

--- src/cms/preview-templates/AboutPagePreview.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AboutPagePreview = ({ widgetFor }) => <div>{widgetFor('body')}</div>;
-
-export default AboutPagePreview;
--- src/cms/preview-templates/BlogPostPreview.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const BlogPostPreview = ({ widgetFor }) => <div>{widgetFor('body')}</div>;
-
-export default BlogPostPreview;
