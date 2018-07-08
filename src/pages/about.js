2c4472f47dfafeeacbc854097c4699f4ad8ec077
Ivan Čurić <ivan.curic@gmail.com>
2018-07-08 14:06:54 +0200

    New pages added

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <section>
+    <div className="container">
+      <h1>About the tournament</h1>
+
+      <h2>Categories and opportunities</h2>
+
+      <h3>Longsword</h3>
+      <ul>
+        <li>Open tournament for both men and women</li>
+        <li>Longsword category</li>
+        <li>Up to 36 participants</li>
+      </ul>
+
+      <h3>One-handed swords</h3>
+      <ul>
+        <li>􏰀Open tournament for both men and women</li>
+        <li>One-handed sword category includes sideswords and sabres</li>
+        <li>Up to 12 participants</li>
+      </ul>
+
+      <h3>Sparring pits</h3>
+      <ul>
+        <li>
+          􏰀Sparring pits are extra fighting areas where all participants can
+          fight freely
+        </li>
+        <li>􏰀Results do not impact tournament</li>
+        <li>􏰀With or without judicial overview</li>
+        <li>􏰀Available throughout the entire event</li>
+      </ul>
+    </div>
+  </section>
+);
+
+export default AboutPage;
---
+++ src/pages/register.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const RegisterPage = () => (
+  <section>
+    <div className="container">
+      <h1>Registration</h1>
+
+      <h2>Entrance fee</h2>
+      <p>
+        The entrance fee for all contestants is 30€. Lunch is included in the
+        price. There is no entrance fee for non-contestants. Lunch is paid
+        separately.
+      </p>
+
+      <h2>Accommodation price per day</h2>
+      <ul>
+        <li>Single-bed room - 170HRK(~23€)</li>
+        <li>Double-bed room - 140HRK(~19€) per bed</li>
+      </ul>
+      <p> The price includes breakfast.</p>
+      <p>
+        All participants must pay their entrance fee and their total price for
+        the accommodations in order to compete.
+      </p>
+
+      <h2>Dinner</h2>
+      <p>
+        Celebratory dinner will be held on the evening of 25th of august. The
+        price is 10€.
+      </p>
+    </div>
+  </section>
+);
+
+export default RegisterPage;
