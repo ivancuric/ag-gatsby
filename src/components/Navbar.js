f9a2adf3c467b491dc59e438ac08308be49be697
Ivan Čurić <ivan.curic@gmail.com>
2018-07-10 00:07:33 +0200

    Register

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+const Image = ({ src, alt }) => {
+  const baseUrl =
+    'https://res.cloudinary.com/ars-gladiatoria-zagreb/image/upload';
+  const size = 200;
+  const params = ['c_fill', 'g_auto', 'f_auto', 'q_auto:best'].join(',');
+  return <img src={`${baseUrl}/${params},w_${size}/${src}`} alt={alt} />;
+};
+
 const Navbar = () => (
   <header className="navbar">
-    <div className="container">
-      <nav className="nav">
-        <Link to="/">Homepage</Link>
-        <Link to="/about">About</Link>
-        <Link to="/schedule">Schedule</Link>
-        <Link to="/rules">Rules</Link>
-        <Link to="/team">Team</Link>
-        <Link to="/register">Register</Link>
-      </nav>
-    </div>
+    <nav className="nav">
+      <Link className="logo" to="/">
+        <Image src="v1531172911/logos/NeCro-LOGO.png" />
+      </Link>
+      <Link to="/about">About</Link>
+      <Link to="/schedule">Schedule</Link>
+      <Link to="/rules">Rules</Link>
+      <Link to="/team">Team</Link>
+      <Link className="register-button" to="/register">Register</Link>
+    </nav>
   </header>
 );
 
