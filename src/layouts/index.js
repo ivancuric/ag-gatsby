e97520717c99bcc77e1a4410924c012a551257e4
Ivan Čurić <ivan.curic@gmail.com>
2018-07-11 22:52:21 +0200

    move webfontloader to didmount

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,22 +4,37 @@ import '../styles/main.scss';
 import Navbar from '../components/Navbar';
 import WebFont from 'webfontloader';
 
-WebFont.load({
-  google: {
-    families: ['Lora:400,700', 'IBM Plex Serif:400,500,600,700'],
-  },
-});
+export default class Layout extends React.Component {
+  constructor({ children, data }) {
+    super();
+    this.children = children;
+    this.data = data;
+  }
 
-export default ({ children, data }) => (
-  <React.Fragment>
-    <Helmet>
-      <title>{data.site.siteMetadata.title}</title>
-      <meta name="description" content={data.site.siteMetadata.description} />
-    </Helmet>
-    <Navbar />
-    <main>{children()}</main>
-  </React.Fragment>
-);
+  componentDidMount() {
+    WebFont.load({
+      google: {
+        families: ['Lora:400,700', 'IBM Plex Serif:400,500,600,700'],
+      },
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>{this.data.site.siteMetadata.title}</title>
+          <meta
+            name="description"
+            content={this.data.site.siteMetadata.description}
+          />
+        </Helmet>
+        <Navbar />
+        <main>{this.children()}</main>
+      </React.Fragment>
+    );
+  }
+}
 
 export const query = graphql`
   query LayoutQuery {
